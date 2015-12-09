356ca3fb97a614037486bceaaa6fc646afdc19b3
wangbo01 <wangbo01@huoyunren.com>
2015-12-09 23:26:16 +0800

    TSP to VRP！
    & Add google map test

--- tsp/TSP.py
+++ tsp/TSP.py
@@ -12,16 +12,17 @@ import time
 import Tkinter
 import threading
 from GA import GA
-
+from Life import Life
 
 class MyTSP(object):
     "TSP"
 
-    def __init__(self, root, width=800, height=600, n=32):
+    def __init__(self, root, width=800, height=600, n=100):
         self.root = root
         self.width = width
         self.height = height
         self.n = n
+        self.condition = 2000
         self.canvas = Tkinter.Canvas(
             root,
             width=self.width,
@@ -56,6 +57,14 @@ class MyTSP(object):
         self.clear()
         self.nodes = []  # 节点坐标
         self.nodes2 = []  # 节点图片对象
+        x, y = self.width / 2, self.height / 2
+        self.start = (x, y)
+        self.canvas.create_oval(x - self.__r,
+                                           y - self.__r, x + self.__r, y + self.__r,
+                                           fill="#00ff00",
+                                           outline="#000000",
+                                           tags="O",
+                                           )
         for i in range(self.n):
             x = random.random() * (self.width - 60) + 30
             y = random.random() * (self.height - 60) + 30
@@ -78,7 +87,10 @@ class MyTSP(object):
             save=self.save()
         )
         self.order = range(self.n)
-        self.line(self.order)
+        init_life = Life(self, self.order)
+        jg = self.judge()
+        jg(init_life)
+        self.line(init_life)
 
     def distance(self, order):
         "得到当前顺序下连线总长度"
@@ -100,8 +112,34 @@ class MyTSP(object):
 
     def judge(self):
         "评估函数"
-        return lambda lf, av=100: 1.0 / self.distance(lf.gene)
-
+        def f(lf, av=100):
+            lf.separate_indexs = []
+            path = lf.gene
+            path_len = len(lf.gene)  # 路径长度，不包括起点
+            i = 0  # 路径中 node 的指针
+            p1 = self.start
+            total_distance = 0
+            one_distance = 0
+            while i < path_len:
+                p2 = self.nodes[path[i]]
+                distance = math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
+                if one_distance + distance > self.condition:
+                    # 重置迭代变量
+                    if p1 == self.start:
+                        raise RuntimeError("point{}far from{}".format(p2, p1))
+                    one_distance = 0
+                    p1 = self.start
+                    lf.separate_indexs.append(i)
+                else:
+                    p1 = p2
+                    one_distance += distance
+                    total_distance += distance
+                    i += 1
+            lf.set_distance(total_distance)
+            lf.separate_indexs.append(i)
+            return 1 / (len(lf.separate_indexs) * 1600 + total_distance)
+        # return lambda lf, av=100: 1.0 / self.distance(lf.gene)
+        return f
     def xFunc(self):
         "交叉函数"
 
@@ -142,22 +180,26 @@ class MyTSP(object):
 
         while self.__running:
             self.ga.next()
-            self.line(self.ga.best.gene)
+            self.line(self.ga.best)
             self.title("TSP - gen: %d" % self.ga.generation)
             self.canvas.update()
 
         self.__t = None
 
-    def line(self, order):
-        "将节点按 order 顺序连线"
+    def line(self, life):
+        order = life.gene
+        "将节点按 order 顺序连线 以life.separete_indexs 分割"
         self.canvas.delete("line")
 
         def line2(i1, i2):
-            p1, p2 = self.nodes[i1], self.nodes[i2]
+            p1 = self.start if i1 == -1 else self.nodes[i1]
+            p2 = self.nodes[i2]
             self.canvas.create_line(p1, p2, fill="#000000", tags="line")
             return i2
-
-        reduce(line2, order, order[-1])
+        start_number = 0
+        for i in life.separate_indexs:
+            reduce(line2, order[start_number:i], -1)
+            start_number = i
 
     def clear(self):
         for item in self.canvas.find_all():
